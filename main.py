2ff3e598123980afdc6f9b5c4a5cee6e593f373b
riqalter <riqalter@gmail.com>
2024-05-15 14:54:51 +0700

    simple prompt edit

--- main.py
+++ main.py
@@ -19,39 +19,42 @@ else:
 
 
 generation_config = {
-  "temperature": 1,
+  "temperature": 0.7,
   "top_p": 0.95,
-  "top_k": 0,
+  "top_k": 64,
   "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
 }
-
 safety_settings = [
   {
     "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "BLOCK_NONE",
   },
   {
     "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "BLOCK_NONE",
   },
   {
     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "BLOCK_NONE",
   },
   {
     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "BLOCK_NONE",
   },
 ]
 
-system_instruction = "Anda adalah asisten yang ahli dalam memberikan rekomendasi, tips dan saran untuk menghemat energi dan air di rumah tangga. Tujuan Anda adalah membantu pengguna mengurangi konsumsi energi dan air, serta biaya tagihan, dengan memberikan rekomendasi praktis yang dapat diterapkan dalam kehidupan sehari-hari dan selalu menggunakan bahasa indonesia baik dan benar. Tidak menjawab atau merespon pertanyaan-pertanyaan yang tidak berhubungan dengan penghematan energi dan air."
-
-model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",generation_config=generation_config,system_instruction=system_instruction,safety_settings=safety_settings)
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash-latest",
+  safety_settings=safety_settings,
+  generation_config=generation_config,
+  system_instruction="Anda adalah asisten yang ahli dalam memberikan rekomendasi, tips dan saran untuk menghemat energi dan air di rumah tangga. Tujuan Anda adalah membantu pengguna mengurangi konsumsi energi dan air, serta biaya tagihan, dengan memberikan rekomendasi praktis yang dapat diterapkan dalam kehidupan sehari-hari dan selalu menggunakan bahasa indonesia baik dan benar. Tidak menjawab atau merespon pertanyaan-pertanyaan yang tidak berhubungan dengan penghematan energi dan air. Anda diperbolehkan menggunakan emoji.",
+)
 
 if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat(history=[])
 
-st.title("Chatbot Rekomendasi Hemat Energi dan Air di Rumah Tangga 🏡💡💧")
+st.title("Chatbot Rekomendasi Hemat Energi dan Air di Industri Dan Rumah Tangga 🏡💡💧")
 
 def role_to_streamlit(role):
     if role == "model":
